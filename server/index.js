5f8ea877de79876c824f5ffd748cb8394f39d8cc
Michael Bernardo <michael_bernardo@me.com>
2021-02-11 11:17:15 -0500

    deploy to heroku

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 // initialize Express in project
 const express = require('express');
 const app = express();
+const logger = require("./middleware/logger");
+
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 8080;
+app.use(logger);
+
+const PORT = process.env.PORT || 5000;
 
 const shoppingcartRoute = require('./routes/shoppingcartRoute');
 const commentsRoute = require('./routes/commentsRoute');
@@ -17,7 +21,7 @@ if (process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
 }
 
-// start Express on port 8080
+// start Express on port 5000
 app.listen(PORT, () => {
     console.log(`Server Started on ${PORT}`);
     console.log('Press CTRL + C to stop server');
